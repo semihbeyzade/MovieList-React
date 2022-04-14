9363c6b25f3dbf5b2216a388fdf790a28a1749fc
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-04-14 21:48:11 +0200

    Added movie

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MovieList from './MovieList'
 import SearchBar from './SearchBar'
-import Axios from 'axios'
+import AddMovie from './AddMovie';
 import axios from 'axios';
 
 class App extends React.Component {
@@ -11,14 +11,7 @@ class App extends React.Component {
         searchQuery: ''
     }
 
-    /*  async  componentDidMount(){
-           const baseURL = 'http://localhost:3002/movies'
-           const response = await fetch(baseURL)
-           const data = await response.json()
-           this.setState({movies: data})
-       }
-        */
-
+   
     async componentDidMount() {
         const response = await axios.get("http://localhost:3002/movies")
         this.setState({
@@ -29,44 +22,16 @@ class App extends React.Component {
 
     }
 
-/*      deleteMovie = (movie) => {
-        const newMovieList = this.state.movies.filter(
-            m => m.id !== movie.id
-        )
-
-         // this.setState({movies: newMovieList}) 
-
-        this.setState(state => ({
-            movies: newMovieList
-        }))
-    } */ 
-
- /*    deleteMovie = async (movie) => {
-        const baseURL = `http://localhost:3002/movies/${movie.id}`
-        await fetch(baseURL, {
-            method:"DELETE"
-        })
-        const newMovieList = this.state.movies.filter(
-            m => m.id !== movie.id
-        )
-
-         // this.setState({movies: newMovieList}) 
-
-        this.setState(state => ({
-            movies: newMovieList
-        }))
-    } */
-
 
     deleteMovie = async (movie) => {
-        
+
         axios.delete(`http://localhost:3002/movies/${movie.id}`)
 
         const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
         )
 
-         // this.setState({movies: newMovieList}) 
+        // this.setState({movies: newMovieList}) 
 
         this.setState(state => ({
             movies: newMovieList
@@ -91,29 +56,21 @@ class App extends React.Component {
             }
         )
 
-        return ( <
-            div className = "container" >
-            <
-            div className = "row" >
-            <
-            div className = "col-lg-12" >
-            <
-            SearchBar searchMovieProp = {
-                this.searchMovie
-            }
-            /> <
-            /div> <
-            /div>
+        return (
+            <div className = "container" >
+             <div className = "row" >
+              <div className = "col-lg-12" >
+            <SearchBar 
+            searchMovieProp = {this.searchMovie}/> 
+              </div> 
+             </div>
 
-            <
-            MovieList movies = {
-                filteredMovies
-            }
-            deleteMovieProp = {
-                this.deleteMovie
-            }
-            /> <
-            /div>
+            <MovieList movies = {filteredMovies}
+                       deleteMovieProp = {this.deleteMovie}/>
+
+            <AddMovie />
+
+             </div >
         )
 
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,14 +12,19 @@ class SearchBar extends React.Component{
         return(
 
             <form onSubmit={this.handleFormSubmit}>
-                <div className='form-row mb-5'>
-                    <div className='col-12'>
+                <div className='form-row mb-5 mt-3 d-flex'>
+                    <div className='col-10'>
                         <input 
                            onChange={this.props.searchMovieProp} 
                            className='form-control' placeholder='Seach a movie' 
                            type='text'
                            
                         />
+                    </div>
+                    <div className='col-2 '>
+                        <button type='button'
+                                className='btn btn-md btn-danger float-end'>Add Movie
+                        </button>
                     </div>
                 </div>
             </form>
